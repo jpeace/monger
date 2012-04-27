2ce5d4d796596501934db2b2b0031b57f4f8b971
Jarrod Peace <peace.jarrod@gmail.com>
2012-04-27 12:14:44 -0500

    Adding more debug info

--- lib/monger/mongo/database.rb
+++ lib/monger/mongo/database.rb
@@ -9,27 +9,31 @@ module Monger
       end
 
       def find(type, criteria={})
-        if @config.verbose?
-          puts type.inspect
-          puts criteria.inspect
-        end
         @config.mongo_hooks[:before_read].each {|proc| proc.call(type, criteria)}
+        puts "Mongo Find: #{type.inspect} #{criteria.inspect}" if @config.verbose?
+ 
         @db[type.to_s].find(criteria)
       end
 
       def insert(type, doc, options={})
         @config.mongo_hooks[:before_write].each {|proc| proc.call(type, doc)}
+        puts "Mongo Insert: #{type.inspect} #{doc.inspect}" if @config.verbose?
+
         @db[type.to_s].insert(doc)
         @db.get_last_error
       end
 
       def update(type, doc, options={})
         @config.mongo_hooks[:before_write].each {|proc| proc.call(type, doc)}
+        puts "Mongo Update: #{type.inspect} #{doc.inspect}" if @config.verbose?
+
         @db[type.to_s].update({'_id'=>doc.monger_id}, doc)
         @db.get_last_error if options[:atomic]
       end
 
       def delete(type, criteria, options={})
+        puts "Mongo Delete: #{type.inspect} #{criteria.inspect}" if @config.verbose?
+
         @db[type.to_s].remove(criteria)
         @db.get_last_error if options[:atomic]
       end
