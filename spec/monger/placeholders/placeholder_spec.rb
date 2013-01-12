0454614bde476dada7a73929f7ffd4411661a206
Tim Shelburne <shelburt02@gmail.com>
2013-01-12 17:12:07 -0600

    placeholder tests added and passing

--- spec/monger/placeholders/placeholder_spec.rb
+++ spec/monger/placeholders/placeholder_spec.rb
@@ -47,9 +47,13 @@ describe "Placeholders" do
     end
 
     it "can hydrate upon being accessed" do
-      @blog_post.coauthor.should be_a Monger::Placeholders::LazyReferencePlaceholder
-      @blog_post.coauthor.name.should eq "Jane Smith"
-      @blog_post.coauthor.should be_a Domain::Auth::User
+      @blog_post.comments.should be_a Monger::Placeholders::LazyMappedCollectionPlaceholder
+      @blog_post.comments.length.should eq 3
+      @blog_post.comments.should be_a Array
+      @blog_post.comments[0].should be_a Monger::Placeholders::LazyCollectionReferencePlaceholder
+      @blog_post.comments[0].message.should eq "A comment"
+      @blog_post.comments[0].should be_a Domain::Comment
+      @blog_post.comments[1].should be_a Monger::Placeholders::LazyCollectionReferencePlaceholder
     end
   end
 
@@ -60,22 +64,24 @@ describe "Placeholders" do
     end
 
     it "can hydrate upon being accessed" do
-      @blog_post.coauthor.should be_a Monger::Placeholders::LazyReferencePlaceholder
-      @blog_post.coauthor.name.should eq "Jane Smith"
-      @blog_post.coauthor.should be_a Domain::Auth::User
+      @blog_post.shares.should be_a Monger::Placeholders::EagerInverseCollectionPlaceholder
+      @blog_post.shares[0].name.should eq "Jane Smith"
+      @blog_post.shares[0].should be_a Domain::Auth::User
+      @blog_post.shares[1].should be_a Domain::Auth::User
     end
   end
 
   context "when working with an eager mapped collection" do
 
     before(:each) do
-      @blog_post = api.find_by_id(:blog_post, Database::blog_post_id)
+      @user = api.find_by_id(:user, Database::user_id)
     end
 
     it "can hydrate upon being accessed" do
-      @blog_post.coauthor.should be_a Monger::Placeholders::LazyReferencePlaceholder
-      @blog_post.coauthor.name.should eq "Jane Smith"
-      @blog_post.coauthor.should be_a Domain::Auth::User
+      @user.comments.should be_a Monger::Placeholders::EagerMappedCollectionPlaceholder
+      @user.comments[0].message.should eq "Another comment"
+      @user.comments[0].should be_a Domain::Comment
+      @user.comments[1].should be_a Domain::Comment
     end
   end
 end
